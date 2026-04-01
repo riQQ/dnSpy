334915aeb4f958afe8b164e5917281be4b0d8b20
riQQ <riQQ@users.noreply.github.com>
2026-04-01 22:46:56 +0200

    Add ActivateMiddleClick method to TreeNodeData class

---
+++ dnSpy.Contracts.DnSpy/TreeView/TreeNodeData.cs
@@ -0,0 +1,7 @@
+
+		/// <summary>
+		/// Called when the item is middle-clicked. Returns true if it was handled,
+		/// false otherwise.
+		/// </summary>
+		/// <returns></returns>
+		public virtual bool ActivateMiddleClick() => false;
